e7a2634aab6cbcde9725a2ef3fc228125f0cee0b
mabbuprem <mabbupremkumar@gmail.com>
2022-06-07 08:52:06 +0530

    [prem] FEATURE ADDED:Welcome message

---
+++ LineComparitionProblems.php
@@ -0,0 +1,13 @@
+<?php
+ class LineComparison
+ {
+     function welcomeMessage(){
+         echo "Welcome to line Comparison Computation Problem\n";
+     }
+    
+     }
+     $lineComparison = new LineComparison();
+     $lineComparison->welcomeMessage();
+   
+    
+?>
