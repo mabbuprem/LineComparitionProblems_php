faf4515ef3b2bcbc75349dee2ccc3956eb5022c5
mabbuprem <mabbupremkumar@gmail.com>
2022-06-09 07:44:48 +0530

    [prem] FEATURE ADDED: Comparing lenth

--- LineComparitionProblems.php
+++ LineComparitionProblems.php
@@ -6,7 +6,7 @@
      public $x2;
      public $y1;
      public $y2;
-
+     
      function welcomeMessage(){
          echo "Welcome to line Comparison Computation Problem\n";
      }
@@ -24,7 +24,7 @@
     }
      /**
      * Function to Calculate length of the points
-     * using given formula
+     * using given formula Length of a Line = sqrt( (x2 - x1) ^ 2 + (y2 - y1) ^ 2
      */
     function calculateLength(){
         $this->userInput();
@@ -46,13 +46,29 @@
             echo "\nLength2 is Greater than Length1\n";
         }
     }
-     }
+
+    function compareLengths($length1, $length2)
+    {
+        $length1 = (string) $length1;
+        $length2 = (string) $length2;
+        $compare = strcmp($length1, $length2);
+        if ($compare == 0) {
+            echo "\nBoth have Equal Length\n";
+        } elseif ($compare > 0) {
+            echo "\nLength1 is Greater than Length2\n";
+        } else {
+            echo "\nLength1 is Smaller than Length2\n";
+        }
+    }
+    }
      $lineComparison = new LineComparison();
      $lineComparison->welcomeMessage();
      $length1 = $lineComparison->calculateLength();
      echo "\nLength 1:" . $length1."\n";
      $length2 = $lineComparison->calculateLength();
      echo "\nLength 2:" . $length2;
-     $lineComparison->checkEquality($length1, $length2);
-    
+     //$lineComparison->checkEquality($length1, $length2);
+     $lineComparison->compareLengths($length1,$length2);
+
+     
 ?>
